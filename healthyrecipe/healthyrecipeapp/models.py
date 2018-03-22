730f236f1bcb4384820529daeb73113cf38ac559
chonglu2 <healthyrecipe@cpanel3.engr.illinois.edu>
2018-03-22 15:59:32 -0500

    Finish up all model initializations

--- healthyrecipe/healthyrecipeapp/models.py
+++ healthyrecipe/healthyrecipeapp/models.py
@@ -24,4 +24,25 @@ class User(models.Model):
     age = models.IntegerField(null=True)
     gender = models.CharField(null=True, max_length=100)
     exerciseFreq =  models.IntegerField(null=True)
-    
+
+class Ingredient(models.Model):
+    name = models.CharField(null=True, max_length=100)
+    calorie = models.IntegerField(null=True)
+
+class Recipe(models.Model):
+    ingredients = models.ManyToManyField(Ingredient, through='Quantity')
+    name = models.CharField(null=True, max_length=100)
+    calorie = models.IntegerField(null=True)
+    prep_time = models.IntegerField(null=True)
+    direction = models.TextField(null=True)
+
+class Quantity(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=True)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    rating = models.IntegerField(null=True)
+    content = models.TextField(null=True)
