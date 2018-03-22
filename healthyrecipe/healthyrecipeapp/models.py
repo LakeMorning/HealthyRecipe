74a12c77cbbf835d0726c9b2b213bc9cb30d387d
chonglu2 <healthyrecipe@cpanel3.engr.illinois.edu>
2018-03-21 19:15:56 -0500

    1)create user model 2)insert/update/delete/select raw queries success

--- healthyrecipe/healthyrecipeapp/models.py
+++ healthyrecipe/healthyrecipeapp/models.py
@@ -1,3 +1,27 @@
+'''
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Posts(models.Model):
+  title = models.CharField(max_length=200)
+  body = models.TextField()
+  created_at = models.DateTimeField(default=datetime.now, blank=True)
+  def __str__(self):
+    return self.title
+  class Meta:
+    verbose_name_plural = "Posts"
+'''
+
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userName = models.CharField(null=True, max_length=100)
+    password = models.CharField(null=True, max_length=100)
+    height = models.IntegerField(null=True)
+    weight = models.IntegerField(null=True)
+    age = models.IntegerField(null=True)
+    gender = models.CharField(null=True, max_length=100)
+    exerciseFreq =  models.IntegerField(null=True)
+    
--- healthyrecipe/healthyrecipeapp/views.py
+++ healthyrecipe/healthyrecipeapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
 
 # Create your views here.
 def index(request):
     #return HttpResponse('HELLO THERE')
-    return render(request, 'healthyrecipeapp/index.html', {
-        'title': 'Test Jinja Template'
-    })
+    
+    cursor = connection.cursor()
+    
+    #insert
+    # cursor.execute("INSERT INTO healthyrecipeapp_user(userName, password, height, weight, age, gender, exerciseFreq) VALUES('cl', '123', 180, 165, 23, 'male', 5)")
+    
+    #select
+    cursor.execute("SELECT * FROM healthyrecipeapp_user")
+    testUsers = cursor.fetchall()
+    
+    #delete
+    
+    context = {
+        'title': 'Test Jinja Template',
+        'users': testUsers
+    }
+    
+    return render(request, 'healthyrecipeapp/index.html', context)
